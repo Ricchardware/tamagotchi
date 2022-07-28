0c98fabe795d72fb70cb4c9995900c46e10c4e9c
ricchardware <donaddio.riccardo@hotmail.com>
2022-07-29 01:45:20 +0200

    Annamo co sto tamagotchi

--- src/main/java/com/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/tamagotchi/TamagotchiApplication.java
@@ -1,4 +1,4 @@
-package com.tamagotchi.tamagotchi;
+package com.tamagotchi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
